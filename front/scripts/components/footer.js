733f533367484a56665270e3a1c386989e66a089
Thomas Peugnet <thomas.peugnet.dev@gmail.com>
2023-05-18 15:21:05 +0200

    Adding all products

--- front/scripts/components/footer.js
+++ front/scripts/components/footer.js
@@ -6,7 +6,7 @@ export default function create_footer()
     let small_footer = document.createElement("small");
     small_footer.classList.add("text-muted");
     h3_footer.innerHTML = "Kiventout";
-    small_footer.innerHTML = "- Copyrigth 2023";
+    small_footer.innerHTML = "- Copyright 2023";
     h3_footer.appendChild(small_footer);
     div_footer.appendChild(h3_footer);
     return div_footer;
